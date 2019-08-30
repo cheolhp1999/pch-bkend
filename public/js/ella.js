4bf448c65df428c1779b39141507a23f361c9c77
cheolhp1999 <cheolhp1999@gmail.com>
2019-08-30 19:03:16 +0900

    ff

--- public/js/ella.js
+++ public/js/ella.js
@@ -1,3 +1,38 @@
-$(document).ready(function () {
-	
+
+$(".topbar > .menu > li").mouseover(function () {
+	$(this).children(".submenu").stop().slideDown();
 });
+$(".topbar > .menu > li").mouseleave(function () {
+	$(this).children(".submenu").stop().slideUp();
+});
+
+var now = 0;
+var next = now+1;
+var imgs = 2
+var depth = 10;
+var interval;
+
+start();
+
+function start(){
+	$(".slides > img").css({"left":"-100%"});
+	$(".slides > img").eq(now).css({"left":0});
+	interval = setInterval(slide, 3000);
+};
+
+function slide() { 
+	$(".slides > img").eq(next).css("z-index", depth++).stop().animate({"left":0},500,
+	function(){
+		$(this).siblings().css({"left": "-100%"});
+		next==imgs? next=0: next++;
+	});
+ }
+
+
+
+
+// $(document).ready(function () {
+	
+// });
+
+
